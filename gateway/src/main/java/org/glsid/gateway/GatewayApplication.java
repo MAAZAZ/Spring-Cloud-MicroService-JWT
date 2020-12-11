f5d1e7476229c79e594e27fa67fc35c01d5a90ff
MAAZAZ <zakitche@gmail.com>
2020-12-11 13:10:30 +0100

    first commit

---
+++ gateway/src/main/java/org/glsid/gateway/GatewayApplication.java
@@ -0,0 +1,33 @@
+package org.glsid.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+/*
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("r1",r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2",r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }*/
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties prp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,prp);
+    }
+}
+
